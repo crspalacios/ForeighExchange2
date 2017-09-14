0ac47af9161114fa2cea21a5f060090f39daa514
crspalacios <cpalacios@crealodigital.com>
2017-09-13 19:51:50 -0500

    Errors

--- ForeighExchange2/ForeighExchange2/ViewModels/MainViewModel.cs
+++ ForeighExchange2/ForeighExchange2/ViewModels/MainViewModel.cs
@@ -1,17 +1,25 @@
 ﻿using ForeighExchange2.Models;
 using GalaSoft.MvvmLight.Command;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace ForeighExchange2.ViewModels
 {
-    public class MainViewModel
+ 
+    public class MainViewModel : INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
         #region Properties
         public string Amount
         {
@@ -20,8 +28,20 @@ namespace ForeighExchange2.ViewModels
         }
         public ObservableCollection<Rate> Rates
         {
-            get;
-            set;
+            get
+            {
+                return _rates;
+            }
+            set
+            {
+                if (_rates != value)
+                {
+                    _rates = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(_result)));
+                }
+            }
         }
 
         public Rate SourceRate
@@ -38,26 +58,111 @@ namespace ForeighExchange2.ViewModels
 
         public bool IsRunning
         {
-            get;
-            set;
+            get
+            {
+                return _isRunning;
+            }
+            set
+            {
+                if (_isRunning != value)
+                {
+                    _isRunning = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(IsRunning)));
+                }
+            }
         }
 
         public bool isEnabledd
         {
-            get;
-            set;
+            get
+            {
+                return _isEnabled;
+            }
+            set
+            {
+                if (_isEnabled != value)
+                {
+                    _isEnabled = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(_result)));
+                }
+            }
         }
 
         public String Result
         {
-            get;
-            set;
+            get
+            {
+                return _result;
+            }
+            set
+            {
+                if (_result != value)
+                {
+                    _result = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(_result)));
+                }
+            }
         }
         #endregion
+
+        #region Attributes
+        bool _isRunning;
+        bool _isEnabled;
+        string _result;
+        ObservableCollection<Rate> _rates;
+        #endregion
+
+        #region Constructor
+
+
         public MainViewModel()
         {
-            
+            LoadRates();
         }
+
+     
+        #endregion
+
+        #region Methods
+        async void LoadRates()
+        {
+            IsRunning = true;
+            Result = "Loading rates...";
+
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("http://apiexchangerates.azurewebsites.ne");
+                var controller = "/api/Rates";
+                var response = await client.GetAsync(controller);
+                var result = await response.Content.ReadAsByteArrayAsync();
+                //string resultstirng = BitConverter.ToString(result);
+                if (!response.IsSuccessStatusCode)
+                {
+                    IsRunning = false;
+                    Result = result;
+                }
+                var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+                Rates = new ObservableCollection<Rate>(rates);
+
+                IsRunning = false;
+                isEnabledd = true;
+                Result = "Ready to convert!";
+            }
+            catch (Exception ex)
+            {
+                IsRunning = false;
+                Result = ex.Message;
+            }
+        }
+        #endregion
+        
         #region Commands
         public ICommand ConvertCommand
         {
